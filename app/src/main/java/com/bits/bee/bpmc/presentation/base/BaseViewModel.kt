8a4eb041d51f1f1f1a4d2a2e397f7e2b1a74a4b5
aldi <agoy587@gmail.com>
2022-04-28 14:30:26 +0700

    revisi

--- app/src/main/java/com/bits/bee/bpmc/presentation/base/BaseViewModel.kt
+++ app/src/main/java/com/bits/bee/bpmc/presentation/base/BaseViewModel.kt
@@ -18,7 +18,7 @@ abstract class BaseViewModel<State, UIEvent>(
     val _state : MutableStateFlow<State?> = MutableStateFlow(null)
     fun viewStates(): StateFlow<State?> = _state
 
-    protected var state : State
+    var state : State
         get() = _state.value
             ?: throw UninitializedPropertyAccessException("state was queried before being initialized")
         set(value) {
